ef0a5df41581f7c15d168b00e3adf9fe1530964e
elliottneedham <elliottneedham590@gmail.com>
2025-04-13 16:41:09 +0000

    🔧 Point getFabricData to deployed Azure API

--- lib/getFabricData.ts
+++ lib/getFabricData.ts
@@ -1,5 +1,5 @@
 export const getFabricData = async () => {
-  const res = await fetch("/api/nor");
+  const res = await fetch("https://fabric-api-elliott-win.azurewebsites.net/api/nor");
   const data = await res.json();
   return data;
 };
